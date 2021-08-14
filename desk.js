9d962c94618d31c3f1cdac908f4e7f26c7d0c4de
umicar <77049691+umicar@users.noreply.github.com>
2021-08-15 00:05:30 +0530

    Add files via upload

---
+++ desk.js
@@ -0,0 +1,31 @@
+img = "";
+status = "";
+function preload() {
+    img = loadImage("desk.jpg");
+}
+
+function setup() {
+    canvas = createCanvas(600, 450);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded() {
+    console.log("Model Loaded");
+    status=true;
+    objectDetector.detect(img,gotResult);
+}
+
+function gotResult(error,results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+    }
+}
+
+function back(){
+    window.location="index.html";
+}
